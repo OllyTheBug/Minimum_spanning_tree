1d23979e501a3eeb99b15e4b8f336ac3464b2563
OllyTheBug <mendelse@oregonstate.edu>
2022-07-26 19:27:33 -0700

    Plot by direction apparently functional

--- maze_generator_python.py
+++ maze_generator_python.py
@@ -16,7 +16,8 @@ class Maze():
         self.adjacency_matrix = [[-1 for x in range(size**2)] for y in range(size**2)]
         self.vertc = size * size
         self.connect_adjacent_nodes_in_grid()
-        self.p = self.MST_by_prims()
+        self.P = self.MST_by_prims()
+        self.directions = self.parent_array_to_directions()
     def connect_adjacent_nodes_in_grid(self):
         for vertex in range(self.vertc):
             x = vertex % self.size
@@ -29,7 +30,7 @@ class Maze():
                 self.adjacency_matrix[vertex][vertex - self.size] = randint(1, 50)
             if y < self.size - 1:
                 self.adjacency_matrix[vertex][vertex + self.size] = randint(1, 50)
-    def plot_mst(self, P):
+    def plot_mst(self):
         # draw grid of points
         x = [i % self.size for i in range(self.vertc)]
         y = [i // self.size for i in range(self.vertc)]
@@ -37,24 +38,79 @@ class Maze():
         # draw edges
         for i in range(self.vertc):
             # Draw edge from i to P[i]
-            if P[i] is not None:
-                plt.plot([i % self.size, P[i] % self.size], [i // self.size, P[i] // self.size], color='r')
+            if self.P[i] is not None:
+                plt.plot([i % self.size, self.P[i] % self.size], [i // self.size, self.P[i] // self.size], color='r')
             
         plt.show()
         
-    def plot_mst_animated(self, P, direct):
+    def parent_array_to_directions(self):
+        directions = []
+        for i in range(self.vertc):
+            if self.P[i] is not None:
+                x = i % self.size
+                y = i // self.size
+                p_x = self.P[i] % self.size
+                p_y = self.P[i] // self.size
+                if x == p_x:
+                    if y > p_y:
+                        directions.append('U')
+                        print(f'{self.P[i]} moves up to {i}')
+                    else:
+                        directions.append('D')
+                        print(f'{self.P[i]} moves down to {i}')
+                elif y == p_y:
+                    if x < p_x:
+                        directions.append('L')
+                        print(f'{self.P[i]} moves left to {i}')
+                    else:
+                        directions.append('R')
+                        print(f'{self.P[i]} moves right to {i}')
+            else:
+                directions.append('n')
+                print(f'{self.P[i]} does not move to {i}')
+                        
+        return directions
+        
+    def plot_mst_animated(self):
         x = [i % self.size for i in range(self.vertc)]
         y = [i // self.size for i in range(self.vertc)]
         plt.scatter(x, y, s=15)
-        for i in range(len(P)):
-            if P[i] is not None:
-                plt.plot([i % self.size, P[i] % self.size], [i // self.size, P[i] // self.size], color='r')
+        for i in range(len(self.P)):
+            if self.P[i] is not None:
+                plt.plot([i % self.size, self.P[i] % self.size], [i // self.size, self.P[i] // self.size], color='r')
                 # write direction direct[i] at point
                 #plt.text((i % self.size) + 0.5, (i // self.size) + 0.5, direct[i], fontsize=10)
+                print(f'{self.P[i]} moves to {i}')
                     
                 plt.pause(1)
         plt.show()
         
+        
+    def plot_by_directions(self):
+        x = [i % self.size for i in range(self.vertc)]
+        y = [i // self.size for i in range(self.vertc)]
+        plt.scatter(x, y, s=15)
+        # label each point with its id
+        for i in range(self.vertc):
+            plt.text((i % self.size) + 0.2, (i // self.size) + 0.2, str(i), fontsize=10)
+        for i in range(len(self.P)):
+            if self.P[i] is not None:
+                if self.directions[i] == 'D':
+                    print(f'{self.P[i]} moves down')
+                    plt.plot([self.P[i] % self.size, self.P[i] % self.size], [self.P[i] // self.size,self.P[i] // self.size - 1], color='r')
+                elif self.directions[i] == 'R':
+                    print(f'{self.P[i]} moves right')
+                    plt.plot([self.P[i] % self.size, self.P[i] % self.size + 1], [self.P[i] // self.size, self.P[i] // self.size], color='r')
+                elif self.directions[i] == 'U':
+                    print(f'{self.P[i]} moves up')
+                    plt.plot([self.P[i] % self.size, self.P[i] % self.size], [self.P[i] // self.size, self.P[i] // self.size + 1], color='r')
+                elif self.directions[i] == 'L':
+                    print(f'{self.P[i]} moves left')
+                    plt.plot([self.P[i] % self.size, self.P[i] % self.size - 1], [self.P[i] // self.size, self.P[i] // self.size], color='r')
+                
+            else:
+                print('at start')
+        plt.show()
             
     def MST_by_prims(self):
         # each node is a vertex, and has ID (size*y + x)
@@ -97,16 +153,30 @@ class Maze():
                         P[v] = u
         # return the path
         return P
-
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((screen_len, screen_len))
-pygame.display.set_caption('Maze')
-clock = pygame.time.Clock()
-
-
 maze = Maze(maze_len)
-p = maze.p
+print(maze.directions)
+maze.plot_by_directions()
+
+
+pass
+# # pygame setup
+# pygame.init()
+# screen = pygame.display.set_mode((screen_len, screen_len))
+# pygame.display.set_caption('Maze')
+# clock = pygame.time.Clock()
+
+# running = True
+# while running:
+#     clock.tick(60)
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             running = False
+#     screen.fill((0, 0, 0))
+
+    
+#     pygame.display.flip()
+
+
 
 
 pass
